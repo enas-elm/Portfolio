4182122652c937c5810c8a25fb8dd2d4bcc0711f
Enas <enas.elmershati@gmail.com>
2021-11-02 22:11:31 +0100

    Commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+#Portfolio
+
+Voici mon portfolio qui vous fait part de mes compétences et de certains projets que j'ai pu réaliser.
+<br><br>
+(Petit problème au niveau du JS cependant ^^ Je n'ai pas pu regrouper plusieurs même fonction en une)
---
+++ script/script.js
@@ -0,0 +1,84 @@
+/* J'ai essayer de faire une fonction pour toutes les compétences mais sans succès
+
+  function apparait() {
+    var $this= $(this);
+    var parent = $this.parent("li");
+    var x = parent.children('div');
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+*/
+function htmlcss() {
+  var x = document.getElementById("html_css");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function javascript() {
+  var x = document.getElementById("javascript");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function phpsql() {
+  var x = document.getElementById("php_sql");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function python() {
+  var x = document.getElementById("python");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function figma() {
+  var x = document.getElementById("figma");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function xd() {
+  var x = document.getElementById("xd");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function dreamweaver() {
+  var x = document.getElementById("dreamweaver");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function photoshop() {
+  var x = document.getElementById("photoshop");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
